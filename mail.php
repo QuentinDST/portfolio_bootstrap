9ae74f5fd75a01851a1b08799e429931527e8403
QuentinDST <83666778+QuentinDST@users.noreply.github.com>
2022-11-29 16:00:55 +0100

    commit 11
    Ajout fonction mail php

---
+++ mail.php
@@ -0,0 +1,5 @@
+<?php
+    $retour = mail('quentin.dstre@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
