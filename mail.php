78fae064578558b40c53261087d7c79eadc2ddcc
Quentin <83666778+QuentinDST@users.noreply.github.com>
2022-12-06 11:14:31 +0100

    Commit 18
    Ajout animation background

--- mail.php
+++ mail.php
@@ -1,6 +1,8 @@
 <?php
-echo $_POST['message'];
-    $retour = mail('quentin.dstre@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
+$data = array(
+    "mail" => … // votre adresse mail
+    "objet" => … // sujet du message sous le format : « NOM , DATE(JJ/MM) : [OBJET] »
+    "message" => … // contenu du message
+    "isHTML" => 'true'
+   );
 ?>
